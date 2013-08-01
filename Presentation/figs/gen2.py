12dfa47743ab8732f76e2855dbf821cf6362cd8d
Ziyu Wang <ziyucwang@gmail.com>
2013-08-01 10:03:40 -0700

    Added random search.

--- Presentation/figs/gen2.py
+++ Presentation/figs/gen2.py
@@ -22,14 +22,30 @@ path = Path(verts, codes)
 fig = plt.figure()
 ax = fig.add_subplot(111)
 
-x = np.arange(-0.5, 0.5, 0.01)
+#x = np.arange(-0.5, 0.5, 0.01)
+#plt.plot(x, 2*x, linewidth='3')
+#z = np.arange(0.5, 1, 0.01)
+#plt.plot(z, np.ones(z.shape), linewidth='3', color='blue')
 
-plt.plot(x, 2*x, linewidth='3')
-z = np.arange(0.5, 1, 0.01)
-plt.plot(z, np.ones(z.shape), linewidth='3', color='blue')
+#z = np.arange(-1, -0.5, 0.01)
+#plt.plot(z, -np.ones(z.shape), linewidth='3', color='blue')
 
-z = np.arange(-1, -0.5, 0.01)
-plt.plot(z, -np.ones(z.shape), linewidth='3', color='blue')
+grid = True
+if grid:
+    nx, ny = (5, 5)
+    x = np.linspace(-1, 1, nx)
+    y = np.linspace(-1, 1, ny)
+    xv, yv = np.meshgrid(x, y)
+    plt.scatter(xv, yv)
+
+    plt.scatter(x, np.ones(x.shape)*1.1, color='red')
+    plt.scatter(np.ones(y.shape)*1.1, y, color='red')
+else:
+    rr = np.random.rand(25, 2)*2-1
+    plt.scatter(rr[:, 0], rr[:, 1])
+    plt.scatter(rr[:, 0], np.ones(rr[:, 0].shape)*1.1, color='red')
+    plt.scatter(np.ones(rr[:, 1].shape)*1.1, rr[:, 1], color='red')
+
 
 patch = patches.PathPatch(path, facecolor='blue', lw=1, alpha='0.2')
 ax.add_patch(patch)
